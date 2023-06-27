51c10adf07b29e15d54bee94efe95330d84c9fd0
IEgorov <@i.egorov>
2023-06-27 17:35:08 +0300

    feat(api): add graphql

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common'
-
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { AppController } from './app.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { environment } from '../environments/environment'
+import { GraphQLModule } from '@nestjs/graphql'
+import { AppResolver } from './app.resolver'
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({ ...environment.connection }),
+    TypeOrmModule.forRoot({
+      ...environment.connection,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      context: ({ req }) => ({ req }),
+      playground: true,
+    }),
   ],
   controllers: [AppController],
+  providers: [AppResolver],
 })
 export class AppModule {
 }
