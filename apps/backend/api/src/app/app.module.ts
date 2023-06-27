4c46e072b2b0cee19509fc1f4dfd40616e8f1a65
IEgorov <@i.egorov>
2023-06-27 16:23:16 +0300

    feat(db): delete service

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common'
 
 import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { environment } from '../environments/environment'
 
@@ -10,7 +9,6 @@ import { environment } from '../environments/environment'
     TypeOrmModule.forRoot({ ...environment.connection }),
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {
 }
