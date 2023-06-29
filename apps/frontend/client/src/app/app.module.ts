b0d656cc85b689aca10ec86057ef1f5b983c3ae3
IEgorov <@i.egorov>
2023-06-29 11:47:14 +0300

    feat(api): add scss

--- apps/frontend/client/src/app/app.module.ts
+++ apps/frontend/client/src/app/app.module.ts
@@ -4,13 +4,22 @@ import { RouterModule } from '@angular/router'
 import { AppComponent } from './app.component'
 import { appRoutes } from './app.routes'
 import { NxWelcomeComponent } from './nx-welcome.component'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatTooltipModule } from '@angular/material/tooltip'
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
   ],
+  exports: [MatCardModule, MatButtonModule, MatIconModule, MatTooltipModule],
   providers: [],
   bootstrap: [AppComponent],
 })
