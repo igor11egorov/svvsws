33bcbf0ea1a92ef172a5d451fdfdecb7533f32a3
IEgorov <@i.egorov>
2023-07-04 18:31:35 +0300

    feat(ws) add interfaces

---
+++ apps/backend/api/src/app/users/guards/gql.auth-guard.ts
@@ -0,0 +1,13 @@
+import { AuthGuard } from '@nestjs/passport'
+import { Injectable } from '@angular/core'
+import { ExecutionContext } from '@nestjs/common'
+import { GqlExecutionContext } from '@nestjs/graphql'
+
+@Injectable()
+export class GqlAuthGuard extends AuthGuard('jwt') {
+  getRequest(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context)
+
+    return ctx.getContext().req
+  }
+}
