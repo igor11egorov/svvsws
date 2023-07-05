086b91e39475f5422ea5e36c606137914d397312
IEgorov <@i.egorov>
2023-07-05 16:25:29 +0300

    feat(api-auth) add auth module

---
+++ apps/backend/api/src/app/auth/resolvers/auth.resolves.ts
@@ -0,0 +1,25 @@
+import { Query, Resolver } from '@nestjs/graphql'
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { ISignAuthPayload, ISignAuthResponse } from '@svvsws/shared/data-access/interfaces'
+import { SignIn } from '../decorators/auth.decorators'
+import { AuthService } from '../services/auth.service'
+
+
+@Resolver
+export class AuthResolves {
+  constructor(
+    private readonly authService: AuthService,
+  ) {
+  }
+
+  @Query('login')
+  async login(@SignIn() signInPayload: ISignAuthPayload): Promise<ISignAuthResponse> {
+    return await this.authService.login(signInPayload)
+
+  }
+
+  @Query('logout')
+  async logout(): Promise<boolean> {
+    return true
+  }
+}
---
+++ apps/backend/api/src/app/auth/services/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core'
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { ISignAuthPayload, ISignAuthResponse } from '@svvsws/shared/data-access/interfaces'
+
+@Injectable()
+export class AuthService {
+  login(signInPayload: ISignAuthPayload): Promise<ISignAuthResponse> {
+    return
+  }
+
+}
