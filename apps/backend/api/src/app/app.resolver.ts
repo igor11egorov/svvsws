582ae289b4c379ab9f89fd4b87ec9f86eab9ae08
IEgorov <@i.egorov>
2023-07-06 13:40:59 +0300

    feat(api-auth) auth module

--- apps/backend/api/src/app/app.resolver.ts
+++ apps/backend/api/src/app/app.resolver.ts
@@ -1,6 +1,28 @@
-import { Resolver } from '@nestjs/graphql'
+import GraphQLJSON from 'graphql-type-json'
+import { GraphQLScalarType } from 'graphql'
+import { Kind } from 'graphql/language'
 
-@Resolver('app')
-export class AppResolver {
-
+/**
+ * Resolve scalar type Date & JSON
+ */
+export const resolverMap = {
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom scalar type',
+    serialize(value: Date) {
+      // TODO
+      return value.toISOString() // Value send to the client
+    },
+    parseValue(value: string) {
+      return new Date(value) // Value from the client
+    },
+    parseLiteral(ast) {
+      // TODO
+      if (ast.kind === Kind.STRING) {
+        return new Date(ast.value) // ast value is always in string format
+      }
+      return null
+    },
+  }),
+  JSON: GraphQLJSON,
 }
