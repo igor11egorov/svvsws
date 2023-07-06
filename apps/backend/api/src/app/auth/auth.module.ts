f012cdd08688d367f0f80b7b84a4c07611c25640
IEgorov <@i.egorov>
2023-07-06 13:25:09 +0300

    feat(api-auth) auth module

--- apps/backend/api/src/app/auth/auth.module.ts
+++ apps/backend/api/src/app/auth/auth.module.ts
@@ -1,5 +1,28 @@
 import { Module } from '@nestjs/common'
+import { UserService } from '../users/services/user.service'
+import { PasswordService } from './services/password.service'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { environment } from '../../environments/environment'
+import { AuthService } from './services/auth.service'
+import { JwtStrategy } from './services/jwt.strategy'
+import { AuthResolves } from './resolvers/auth.resolves'
 
-@Module({})
+@Module({
+  imports: [
+    UserService,
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    JwtModule.register({
+      privateKey: environment.jwt.secret,
+      signOptions: {
+        expiresIn: environment.jwt.expiresIn,
+      },
+    }),
+  ],
+  providers: [AuthService, PasswordService, JwtStrategy, AuthResolves],
+  exports: [AuthService, PassportModule],
+})
 export class AuthModule {
 }
